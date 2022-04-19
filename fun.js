9181e9a66789ff4a4b8104d83e0072de9dc703a7
karen <kdescobar21@gmail.com>
2022-04-19 16:51:52 -0400

    promesas

--- fun.js
+++ fun.js
@@ -1,6 +1,8 @@
 const colors= require('colors');
 const fs = require('fs');
 const path = require ('path');
+const https = require('https');
+const url = require('url')
 
 
 
@@ -19,6 +21,56 @@ const xtension = (resp)=> path.extname(resp);
 // leer archivos dentro del directorio 
 const directory = (resp) => fs.readdirSync(resp);
 
+// lee los links dentro del archivo .md 
+const readFile = (resp) => fs.readFile(resp, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('error: ', err);
+    } else {
+      const Regular = /(https?:\/\/)(www\.)?[-a-z0-9@:%._\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-a-z0-9()!@:%_\+.~#?&\/\/=]*)/gi
+      const  url = data.match(Regular);
+      console.log(colors.magenta(url));
+      // aca se le dice en caso de... la promesa haga esto
+      const promesa = validatLink(url[0]);
+      promesa.then((linkstatus)=>{
+          console.log(linkstatus)
+      })
+      console.log(promesa)
+      return url;
+    }
+  });
+  // conocer el status del link por medio de la promesa
+  function validatLink(link) {
+    return new Promise((resolve) => {
+      const options = {
+        method: 'HEAD',
+        host: url.parse(link).host,
+        port: 80,
+        path: url.parse(link).pathname,
+      };
+  
+      const req = https.request(options, (res) => {
+        const Data = {
+          linkname: link,
+          Code: res.statusCode,
+          status: res.statusCode <= 399,
+        };
+        resolve(Data);
+      });
+  //escucha la promesa
+      req.on('error', (error) => {
+        console.error(error);
+        const newData = {
+          linkname: link,
+          status: false,
+        };
+        resolve(newData);
+      });
+  
+      req.end();
+});
+} 
+
+
 
 
 
@@ -27,6 +79,8 @@ module.exports.convertPath=convertPath;
 module.exports.archivos=archivos;
 module.exports.xtension=xtension;
 module.exports.directory=directory;
+module.exports.readFile=readFile;
+module.exports.validatLink=validatLink;
 
 
 
--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-const  fun = require ('./fun.js');
-const path = require ('path');
+const fun = require('./fun.js');
+const path = require('path');
 const fs = require('fs');
-const colors= require('colors');
+const colors = require('colors');
+
 
 
 
 const readline = require('readline');
 const { exit } = require('process');
 const interfazCaptura = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-interfazCaptura.question( colors.rainbow('ingrese la ruta '),function(resp){
-   console.log(colors.yellow(`la ruta ingresada es: ${resp}`));
-   const rutaexi = fun.fileExists(resp)
-   if (rutaexi == false){
-    console.log (colors.bgRed("No existe la ruta"))
+interfazCaptura.question(colors.rainbow('ingrese la ruta '), function (resp) {
+  console.log(colors.yellow(`la ruta ingresada es: ${resp}`));
+  const rutaexi = fun.fileExists(resp)
+  if (rutaexi == false) {
+    console.log(colors.bgRed("No existe la ruta"))
     exit();
   }
   else {
-  console.log(colors.green("La ruta existe:", rutaexi));
-  
+    console.log(colors.green("La ruta existe:", rutaexi));
+
   }
   const absoluta = fun.convertPath(resp)
-  console.log(colors.blue("la ruta absoluta es" +absoluta ));
+  console.log(colors.blue("la ruta absoluta es" + absoluta));
 
-const archiD =fun.archivos(resp);
+  const archiD = fun.archivos(resp);
 
-if (archiD.isFile()){
-console.log(colors.yellow("es un archivo"));
-const xtension = fun.xtension(resp);
- if(xtension ==   '.md'){
-   console.log(colors.america( 'es un archivo .md'));
- }else {
-   console.log(colors.bgRed('No es un archivo .md'));
-   exit();
- }
-}
-if (archiD.isDirectory()){
-  const arrayMd = [];
-  console.log(colors.bgRed("esto es un directorio"));
-  const directory = fun.directory(resp).forEach(files => {
-  const mdExtens = fun.xtension(files)
-  if (mdExtens == '.md'){
-    arrayMd.push(files);
-   }
-  });
-  if (arrayMd == ''){
-    console.log(colors.blue('este directorio no tiene archivos .md'));
-  }else{
-    console.log(colors.rainbow('Tiene archivos con extension .md'));
-    console.log(arrayMd);
+  if (archiD.isFile()) {
+    console.log(colors.yellow("es un archivo"));
+    const xtension = fun.xtension(resp);
+    if (xtension == '.md') {
+      console.log(colors.america('es un archivo .md'));
+       fun.readFile(resp);
+      //fun.validatLink(valLinks[0]);
+    // console.log(valLinks)
+    } else {
+      console.log(colors.bgRed('No es un archivo .md'));
+      exit();
+    }
   }
-  
+  if (archiD.isDirectory()) {
+    const arrayMd = [];
+    console.log(colors.bgRed("esto es un directorio"));
+    const directory = fun.directory(resp).forEach(files => {
+      const mdExtens = fun.xtension(files)
+      if (mdExtens == '.md') {
+        arrayMd.push(files);
+      }
+    });
+    if (arrayMd == '') {
+      console.log(colors.blue('este directorio no tiene archivos .md'));
+    } else {
+      console.log(colors.rainbow('Tiene archivos con extension .md'));
+      console.log(arrayMd);
+    }
+   interfazCaptura.close(); 
 };
-
-  interfazCaptura.close();
-});
-
+})
 
 
